5b45b1a175eb7be33027a1502f74e361eb0b7913
KatiLong <katileighlong@gmail.com>
2018-06-23 16:36:07 -0700

    Setting up basic trigger functions for app outline

--- public/client.js
+++ public/client.js
@@ -2,15 +2,23 @@ const globalObject = {
     name: 'Carmen',
     username: 'CarmenSD',
     password: 'Where?2018',
-    userSpecific: {
-        insulinOnBoard: 0,
+    userSettings: {
+        IOB: {
+            units: 0,
+            timeRemaining: 0
+        },
         iobStatusColor: 'teal',
         insulinMetric: 'units',
-
+        insulinIncrement: 1,
+        bgMeasurementType: 'mg/dl',
+        carbRatio: 9,
+        correctionFactor: 35,
+        targetBG: 110
     },
     methods: {
         iobCalculation: (num) => setTimeout(function(){ alert("IOB Tracking"); }, 3000),
         iobAddUnits: (num) => globalObject.userSpecific.insulinOnBoard + num,
+        iobTimeRemainFormat: () => console.log('Display hours and minutes')
     }
 }
 
@@ -23,8 +31,60 @@ $(document).ready(function(){
     $('.datepicker').datepicker();
     $('.timepicker').timepicker();
     $('select').formSelect();
+
+    $('section').hide();
+    $('#login-page').show();
 });
 
 ///////////////Triggers//////////////
+//User Login
+$('#login-form').submit( (event) => {
+    event.preventDefault();
 
+    $('#login-page').hide();
+    $('#user-dashboard').show();
 
+});
+//Switch User Forms -> Signup
+$('#change-form-signup').click((event) => {
+    event.preventDefault();
+
+    $('#login-page').hide();
+    $('#signup-page').show();
+});
+//Feedback Form
+$('#feedback-trigger').click((event) => {
+    event.preventDefault();
+
+    $('footer').hide();
+    $('#feedback').show();
+});
+//Cancel button Feedback form
+$('.cancel-button').click((event) => {
+    event.preventDefault();
+
+    $('footer').show();
+    $('#feedback').hide();
+});
+//Feedback submit
+$('#feedback-form').submit( (event) => {
+    event.preventDefault();
+
+    $('#feedback').hide();
+    $('#footer').show();
+});
+//User Signup switch form -> Login
+$('#change-form-login').click((event) => {
+    event.preventDefault();
+
+    $('#signup-page').hide();
+    $('#login-page').show();
+});
+//Signup submit
+$('#signup-form').submit( (event) => {
+    event.preventDefault();
+
+    $('#signup-page').hide();
+    $('#user-dashboard').show();
+
+});
