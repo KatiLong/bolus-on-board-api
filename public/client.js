8d4e563d66c084655c914e60b4e90c30366bca0c
KatiLong <katileighlong@gmail.com>
2018-06-23 12:39:59 -0700

    Bolus section complete, blood glucose form and button added.

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
-$(function() {
+$(document).ready(function(){
     console.log( "ready!" );
 
+    $('.datepicker').datepicker();
     $('.timepicker').timepicker();
-    $('select').material_select();
+    $('select').formSelect();
 });
