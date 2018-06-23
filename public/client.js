02ccf43880741f5caa87c8a6c6589b12308ac77f
KatiLong <katileighlong@gmail.com>
2018-06-23 15:09:14 -0700

    Creating object list of info to be tracked

--- public/client.js
+++ public/client.js
@@ -1,3 +1,22 @@
+const globalObject = {
+    name: 'Carmen',
+    username: 'CarmenSD',
+    password: 'Where?2018',
+    userSpecific: {
+        insulinOnBoard: 0,
+        iobStatusColor: 'teal',
+        insulinMetric: 'units',
+
+    },
+    methods: {
+        iobCalculation: (num) => setTimeout(function(){ alert("IOB Tracking"); }, 3000),
+        iobAddUnits: (num) => globalObject.userSpecific.insulinOnBoard + num,
+    }
+}
+
+
+///////////////Document Ready Function///////////
+
 $(document).ready(function(){
     console.log( "ready!" );
 
@@ -5,3 +24,7 @@ $(document).ready(function(){
     $('.timepicker').timepicker();
     $('select').formSelect();
 });
+
+///////////////Triggers//////////////
+
+
