f93c08718878207df5f759abe64272297ace71c7
KatiLong <katileighlong@gmail.com>
2018-06-23 18:57:43 -0700

    Settings reafactor, new triggers

--- public/client.js
+++ public/client.js
@@ -34,6 +34,7 @@ $(document).ready(function(){
 
     $('section').hide();
     $('#login-page').show();
+
 });
 
 ///////////////Triggers//////////////
@@ -43,7 +44,7 @@ $('#login-form').submit( (event) => {
 
     $('#login-page').hide();
     $('#user-dashboard').show();
-
+    $('form').hide();
 });
 //Switch User Forms -> Signup
 $('#change-form-signup').click((event) => {
@@ -58,6 +59,7 @@ $('#feedback-trigger').click((event) => {
 
     $('footer').hide();
     $('#feedback').show();
+    $('#feedback-form').show();
 });
 //Cancel button Feedback form
 $('.cancel-button').click((event) => {
@@ -69,9 +71,9 @@ $('.cancel-button').click((event) => {
 //Feedback submit
 $('#feedback-form').submit( (event) => {
     event.preventDefault();
-
+    alert("Thanks for the Feedback!");
     $('#feedback').hide();
-    $('#footer').show();
+    $('footer').show();
 });
 //User Signup switch form -> Login
 $('#change-form-login').click((event) => {
@@ -86,5 +88,130 @@ $('#signup-form').submit( (event) => {
 
     $('#signup-page').hide();
     $('#user-dashboard').show();
+    $('form').hide();
 
+});
+
+////USER DASHBOARD Triggers/////////
+//Reusable Dashboard Back button
+$('.dash-back').click((event) => {
+    event.preventDefault();
+
+    $('form').hide();
+    $('.dash-button').show();
+});
+
+//Bolus show
+$('#bolus-trigger').click((event) => {
+    event.preventDefault();
+
+    $('.dash-button').hide();
+    $('#bolus-form').show();
+});
+
+//Bolus submit
+$('#bolus-form').submit( (event) => {
+    event.preventDefault();
+
+    $('form').hide();
+    $('.dash-button').show();
+    alert('Form submitted');
+});
+
+//BG show
+$('#bg-trigger').click((event) => {
+    event.preventDefault();
+
+    $('.dash-button').hide();
+    $('#blood-glucose-form').show();
+});
+
+//BG submit
+$('#blood-glucose-form').submit( (event) => {
+    event.preventDefault();
+
+    $('form').hide();
+    $('.dash-button').show();
+    alert('Form submitted');
+});
+
+//Basal show
+$('#basal-trigger').click((event) => {
+    event.preventDefault();
+
+    $('.dash-button').hide();
+    $('#basal-form').show();
+});
+
+//Basal submit
+$('#basal-form').submit( (event) => {
+    event.preventDefault();
+
+    $('form').hide();
+    $('.dash-button').show();
+    alert('Form submitted');
+});
+
+//A1c show
+$('#a1c-trigger').click((event) => {
+    event.preventDefault();
+
+    $('.dash-button').hide();
+    $('#a1c-form').show();
+});
+
+//A1c submit
+$('#a1c-form').submit( (event) => {
+    event.preventDefault();
+
+    $('form').hide();
+    $('.dash-button').show();
+    alert('Form submitted');
+});
+////////////////////////////////////////
+//Settings Section show
+$('#settings-trigger').click((event) => {
+    event.preventDefault();
+
+    $('#user-dashboard').hide();
+    $('.settings-forms').show();
+    $('.settings-div').hide();
+
+    $('#settings').show();
+    $('#settings-menu').show();
+    $('.setting-button').show();
+});
+
+//Settings Show Section Trigger
+$('.setting-button').click(function(event) {
+    event.preventDefault();
+
+    $('.settings-div').hide();
+    $('.setting-button').hide();
+
+    $(this).siblings('div').show();
+});
+
+//Settings Back Button
+$('.back-button').click(function(event) {
+    event.preventDefault();
+
+    $('.settings-div').hide();
+    $('.setting-button').show();
+});
+
+$('.home-button').click(function(event) {
+    event.preventDefault();
+
+    $('#settings').hide();
+    $('#user-dashboard').show();
+});
+
+//
+$('#units-carbs-form').submit( (event) => {
+    event.preventDefault();
+
+    $('form').hide();
+    $('.dash-button').show();
+    alert('Form submitted');
 });
